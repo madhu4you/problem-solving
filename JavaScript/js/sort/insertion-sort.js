9f62d7feb36199f7a7fbd1fc1658790996babc08
Madhusudhana KN <madhusudhanakn@Madhu-MacBook-Pro.local>
2019-12-08 21:05:25 -0800

    Push sorting algo

--- JavaScript/js/sort/insertion-sort.js
+++ JavaScript/js/sort/insertion-sort.js
@@ -10,3 +10,30 @@ var insertionSort = function (item) {
     }
     return item;
 }
+
+let insertionSortArray = function (item, n) {
+    if (n === undefined) n = item.length;
+    if (n <= 1) return item;
+    let j = n - 1;
+    while (j >= 1 && item[j] > item[j + 1]) {
+        let temp = item[j];
+        item[j] = item[j + 1];
+        item[j + 1] = temp;
+        j--;
+    }
+    insertionSortArray(item, n - 1);
+    return item;
+}
+
+function insertionSortElement(item) {
+    for (i = 1; i < item.length; i++) {
+        let temp = item[i];
+        let j = i - 1;
+        while (j >= 0 && item[j] > temp) {
+            item[j + 1] = item[j];
+            j--;
+        }
+        item[j + 1] = temp;
+    }
+    return item;
+}
--- JavaScript/test/sort/insertion-sort.spec.js
+++ JavaScript/test/sort/insertion-sort.spec.js
@@ -8,4 +8,18 @@ describe("Sort the given array by using selection sort", function () {
         output = insertionSort(input);
         expect(output).toEqual(expectedValue);
     });
+
+    it("Output should expect sorted array", function () {
+        input = [3, 2, 1, 4, 6, 5];
+        expectedValue = [1, 2, 3, 4, 5, 6];
+        //output = insertionSortArray(input);
+        //expect(output).toEqual(expectedValue);
+    });
+
+    it("Output should expect sorted array", function () {
+        input = [3, 2, 1, 4, 6, 5];
+        expectedValue = [1, 2, 3, 4, 5, 6];
+        output = insertionSortElement(input);
+        expect(output).toEqual(expectedValue);
+    });
 });
