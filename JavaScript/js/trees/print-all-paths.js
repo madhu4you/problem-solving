54ef53d1e455bfa072787bc4f084ba7b208b88ae
Madhusudhana KN <madhusudhanakn@Madhu-MacBook-Pro.local>
2019-12-18 22:09:13 -0800

    Push tree problem practice.

--- JavaScript/js/trees/print-all-paths.js
+++ JavaScript/js/trees/print-all-paths.js
@@ -1,17 +1,35 @@
-function printAllPaths(root){
-    if(!root) return;
+function printAllPaths(root) {
+    if (!root) return;
     let allPaths = [];
     printHelper(node, path) {
         path.push(node.val);
-        if(!node.left_ptr && !node.right_ptr) {
+        if (!node.left_ptr && !node.right_ptr) {
             allPaths.push(path.slice(0));
         }
-        if(node.left_ptr) printHelper(node.left_ptr);
-        if(node.right_ptr) printHelper(node.right_ptr);
+        if (node.left_ptr) printHelper(node.left_ptr);
+        if (node.right_ptr) printHelper(node.right_ptr);
         path.pop();
     }
     printHelper(root, []);
-    allPaths.forEach(function(path){
+    allPaths.forEach(function (path) {
+        console.log(path.join(" "));
+    });
+}
+
+
+function printBSTPath(root) {
+    let allPath = [];
+
+    function pathHelper(node, path) {
+        if (!node) return;
+        path.push(node.val);
+        if (!node.right_ptr && !node.left_ptr) allPath.push(path.slice(0));
+        pathHelper(node.left_ptr, path);
+        pathHelper(node.right_ptr, path);
+        path.pop();
+    }
+    pathHelper(root, []);
+    allPath.forEach(function (path) {
         console.log(path.join(" "));
     });
 }
