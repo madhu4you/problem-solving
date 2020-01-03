8a5cde9a75b31b21e5764614b305a747123478fc
Madhusudhana KN <madhusudhanakn@Madhu-MacBook-Pro.local>
2020-01-03 07:24:05 -0800

    Recursive mode

--- JavaScript/js/dp/path-of-matrix.js
+++ JavaScript/js/dp/path-of-matrix.js
@@ -41,3 +41,29 @@ function numberOfPaths(matrix) {
     }
     return table[rowSize - 1][colSize - 1];
 }
+
+
+function findMaxPath(currentRow, currentColumn, destRow, destCol) {
+    // Base condition
+    if (currentRow > destRow || currentColumn > destCol) {
+        return 0;
+    }
+    // Successful path found
+    if (currentRow === destRow && currentColumn === destCol) {
+        return 1;
+    }
+    // Finding the number of paths that can be formed from increasing
+    // the current row's Count and Current column's count one after the other.
+    const pathsInRows = findMaxPath(currentRow + 1, currentColumn, destRow, destCol);
+    const pathsInColums = findMaxPath(currentRow, currentColumn + 1, destRow, destCol);
+    return (pathsInRows + pathsInColums);
+}
+
+
+function findMaxPathSrcToDes(rows, cols) {
+    // Initial rows and columns to begin with.0,0 is the first row and col index we are choosing
+    return findMaxPath(0, 0, rows - 1, cols - 1);
+}
+
+const num_of_paths = findMaxPathSrcToDes(3, 3);
+console.log('Number of Paths', num_of_paths);
